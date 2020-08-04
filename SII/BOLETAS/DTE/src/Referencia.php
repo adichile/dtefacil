249452b81ff64000336c1ec5cf70798ebe6a50e6
Joaquín Gamboa <j.gamboa@redcapital.cl>
2020-08-04 01:42:32 -0400

    mejora esquema boelta electrónica

--- SII/BOLETAS/DTE/src/Referencia.php
+++ SII/BOLETAS/DTE/src/Referencia.php
@@ -3,6 +3,8 @@ namespace ADICHILE\DTEFACIL\SII\BOLETAS\DTE;
 
 class Referencia{
     public $NroLinRef;
+    public $TpoDocRef;
+    public $FolioRef;
     public $CodRef;
     public $RazonRef;
     public $CodVndor;
@@ -46,6 +48,38 @@ class Referencia{
 
     function setCodCaja($CodCaja) {
         $this->CodCaja = $CodCaja;
+    }
+
+    /**
+     * @return mixed
+     */
+    function getTpoDocRef()
+    {
+        return $this->TpoDocRef;
+    }
+
+    /**
+     * @param mixed $TpoDocRef
+     */
+    function setTpoDocRef($TpoDocRef)
+    {
+        $this->TpoDocRef = $TpoDocRef;
+    }
+
+    /**
+     * @return mixed
+     */
+    function getFolioRef()
+    {
+        return $this->FolioRef;
+    }
+
+    /**
+     * @param mixed $FolioRef
+     */
+    function setFolioRef($FolioRef)
+    {
+        $this->FolioRef = $FolioRef;
     }
 
     
